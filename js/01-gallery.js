b8e1d60e103b58c97219f912f8bb90875df35b8d
katya20220910 <katyaantonova009@gmail.com>
2022-12-04 14:24:21 +0200

    Up

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,86 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+const galleryArr = iteamsGallery(galleryItems);
+function iteamsGallery(galleryItems) {
+    return galleryItems.map(({ original, preview, description }) => {
+      return  `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+        .join('');
+}
+gallery.insertAdjacentHTML('beforeend', galleryArr);
+
+gallery.addEventListener('click', onOpenModal);
+
+function onOpenModal(event) {
+    event.preventDefault();
+
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
+    const instance = basicLightbox.create(
+        `
+	<img width="1000" height="600" src="${event.target.dataset.source}">
+`,
+        {
+      
+            onShow: instance => {
+                window.addEventListener('keydown', onEsckey);
+            },
+
+            onClose: instance => {
+                window.removeEventListener('keydown', onEsckey);
+            },
+        },
+    );
+    instance.show();
+
+    // function onOpenModal(event) {
+    //   event.preventDefault();
+
+    //     if (event.target.nodeName !== 'IMG') {
+    //         return;
+    //     }
+    //      const instance = basicLightbox.create(`
+    //  	<img src="${event.target.dataset.cource}"
+    //     alt ="${event.target.alt}"
+    //     />`,
+    // {
+    //              onShow: () => {
+    //                  window.addEventListener('keydown', onEsckey);
+    //              },
+
+    //              onClose: () => {
+    //                  window.removeEventListener('keydown', onEsckey);
+    //              },
+    //          }
+    //      )
+    //  .show()
+
+ 
+    //     function onEsckey(event) {
+    //         if (event.code !== "Escape") {
+    //             return;
+    //         } else {
+    //             instance.close();
+    //         }
+    //     }
+    // }
+    function onEsckey(event) {
+        if (event.code === "Escape") {
+            instance.close();
+        }
+    }
+}
+
+// console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,4 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
+new SimpleLightbox('.gallery a', { /* options */ });
 console.log(galleryItems);
